343c13955f3919256b28409786ad6a8fa30b43e6
pinkerton42069 <pramit126@gmail.com>
2023-10-04 20:33:20 +0530

    Next Commit

--- src/BoxV1.sol
+++ src/BoxV1.sol
@@ -1,34 +1,37 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
+
 import {UUPSUpgradeable} from "@openZeppelin/openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import {Initializable} from "@openZeppelin/openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
 import {OwnableUpgradeable} from "@openZeppelin/openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
 
 // To remmember: Storage is stored in the proxy contract and not in the implementation contract
 
-//proxy-> deploys implementation -> call some "initializer " function 
+//proxy-> deploys implementation -> call some "initializer " function
 //Proxy contracts don;t have constructors
 
-contract BoxV1 is Initializable, OwnableUpgradeable,UUPSUpgradeable{
+contract BoxV1 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
-    constructor(){
+    constructor() {
         _disableInitializers(); // This doesn't let any initialization happen. This is same as no constructor, its just more verbose
     }
 
-    function initialize() initializer public{
+    function initialize() public initializer {
         __Ownable_init(); // sets owner to msg.sender
         __UUPSUpgradeable_init(); // doesn't do anything as such but best to have it here. it says this is UUPSUpgradeable contract and should be treated as such
     }
-    function getNumber() external view returns(uint256){
+
+    function getNumber() external view returns (uint256) {
         return number;
     }
 
-    function version() external pure returns(uint256){
+    function version() external pure returns (uint256) {
         return 1;
     }
+
     function _authorizeUpgrade(address newImplementation) internal override {}
-    }
+}
 
 //Proxies point to storage slots and not specific variable names
 //Best practice is to have empty reserved space to allow future versions to add new variables without shifting down storage in the inheritance chain.
--- src/BoxV2.sol
+++ src/BoxV2.sol
@@ -1,19 +1,33 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-contract BoxV2{
+import {UUPSUpgradeable} from "@openZeppelin/openzeppelin-contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import {Initializable} from "@openZeppelin/openzeppelin-contracts-upgradeable/proxy/utils/Initializable.sol";
+import {OwnableUpgradeable} from "@openZeppelin/openzeppelin-contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract BoxV2 is Initializable, OwnableUpgradeable, UUPSUpgradeable {
     uint256 internal number;
 
-    function setNumber(uint256 _number) external{
-        //number = _number;
+    constructor() {
+        _disableInitializers(); // This doesn't let any initialization happen. This is same as no constructor, its just more verbose
     }
 
-    function getNumber() external view returns(uint256){
+    function initialize() public initializer {
+        __Ownable_init(); // sets owner to msg.sender
+        __UUPSUpgradeable_init(); // doesn't do anything as such but best to have it here. it says this is UUPSUpgradeable contract and should be treated as such
+    }
+
+    function setNumber(uint256 _number) external {
+        number = _number;
+    }
+
+    function getNumber() external view returns (uint256) {
         return number;
     }
 
-    function version() external pure returns(uint256){
+    function version() external pure returns (uint256) {
         return 2;
     }
 
+    function _authorizeUpgrade(address newImplementation) internal override {}
 }
